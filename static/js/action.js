b66cddde3bf5be83bf82489eba9b52761293aceb
juan-mendes-DEV <jp436807@gmail.com>
2025-01-24 01:40:27 -0300

    arrumando js

--- static/js/action.js
+++ static/js/action.js
@@ -4,11 +4,19 @@ const buttonn = document.getElementById('dropdownButtoN');
 const menuu = document.getElementById('dropdownMenU');
 
 button.addEventListener('click', () => {
+  event.stopPropagation();
   menu.classList.toggle('show'); // Alterna a classe "show"
+  menuu.classList.remove('show');
 });
 
 buttonn.addEventListener('click', () => {
+  event.stopPropagation();
   menuu.classList.toggle('show'); // Alterna a classe "show"
+  menu.classList.remove('show');
+});
+
+menuu.addEventListener('click', (event) => {
+  event.stopPropagation(); // Impede que o clique dentro do dropdown feche o menu
 });
 
 window.addEventListener('click', (event) => {
